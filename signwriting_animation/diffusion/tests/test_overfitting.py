da39f273815c536544bb46847d4b593e999ec534
Yang Yun <yang.yun@uzh.ch>
2025-05-31 01:24:09 +0200

    Update test_overfitting.py

--- signwriting_animation/diffusion/tests/test_overfitting.py
+++ signwriting_animation/diffusion/tests/test_overfitting.py
@@ -42,7 +42,7 @@ def main():
     # all zero and all one sample
     sample_configs = [
         # zeros
-        #("AS14c20S27106M518x529S14c20481x471S27106503x489", 0.0),
+        ("AS14c20S27106M518x529S14c20481x471S27106503x489", 0.0),
         # ones
         ("AS18701S1870aS2e734S20500M518x533S1870a489x515S18701482x490S20500508x496S2e734500x468", 1.0)
     ]
