61963f549809ccd3e879baf167ff82d07ffd3a31
Hongyan <yang.yun@uzh.ch>
2025-12-25 19:54:02 +0100

    test

--- signwriting_animation/diffusion/tests/test_checkpoints.py
+++ signwriting_animation/diffusion/tests/test_checkpoints.py
@@ -79,7 +79,7 @@ def test_checkpoint():
     CLIP_DENOISED = True
     FIXED_INDICES = [10, 50, 100, 200, 500]
 
-    ckpt_path = "logs/full/checkpoints/last-v1.ckpt"
+    ckpt_path = "logs/full_unfrozen_clip/checkpoints/last.ckpt"
     data_dir = "/home/yayun/data/pose_data/"
     csv_path = "/home/yayun/data/signwriting-animation/data_fixed.csv"
     stats_path = f"{data_dir}/mean_std_178_with_preprocess.pt"
