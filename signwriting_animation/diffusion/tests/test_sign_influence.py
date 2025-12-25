ab2f019b31dac27e70fb593da034f64216d43ba7
Hongyan <yang.yun@uzh.ch>
2025-12-25 19:28:58 +0100

    test

--- signwriting_animation/diffusion/tests/test_sign_influence.py
+++ signwriting_animation/diffusion/tests/test_sign_influence.py
@@ -253,7 +253,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint", type=str, 
-                        default="logs/full/checkpoints/last-v2.ckpt")
+                        default="logs/full_unfrozen_clip/checkpoints/last.ckpt")
     args = parser.parse_args()
     
     test_sign_influence(args.checkpoint)
